9d77dc1604f1821d0ef1f74d987aeb0a97a9f26c
Jason-Hp <107877395+Jason-Hp@users.noreply.github.com>
2025-01-27 20:41:55 +0800

    Add echo feature for Mike.java

--- src/main/java/Mike.java
+++ src/main/java/Mike.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Mike {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         String logo = ".------..------..------..------.\n" +
                 "|M.--. ||I.--. ||K.--. ||E.--. |\n" +
                 "| (\\/) || (\\/) || :/\\: || (\\/) |\n" +
@@ -10,14 +13,30 @@ public class Mike {
                 "`------'`------'`------'`------'";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("_____________________________"+
+        System.out.println("    _____________________________"+
                 "_______________________________\n" +
-                " Hello! I'm Mike!\n" +
-                " What can I do for you?\n" +
-                "_________________________________"+
-                "___________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "_________________________________"+
+                "     Hello! I'm Mike!\n" +
+                "     What can I do for you?\n" +
+                "    _________________________________"+
                 "___________________________\n");
+
+        String userInput = scanner.nextLine();
+        while(!userInput.equalsIgnoreCase("bye")){
+            System.out.println("    _________________________________"+
+                    "___________________________\n"+
+                    "     "+
+                    userInput+
+                    "\n"+
+                    "    _________________________________"+
+                    "___________________________\n");
+            userInput = scanner.nextLine();
+        }
+
+        System.out.println("    _________________________________"+
+                "___________________________\n"+
+                "     Bye. Hope to see you again soon!\n" +
+                "    _________________________________"+
+                "___________________________\n");
+
     }
 }
