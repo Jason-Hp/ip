acb9e8fd911e300dcc68623303e19a99ca8940c3
Jason-Hp <107877395+Jason-Hp@users.noreply.github.com>
2025-02-02 02:42:07 +0800

    Create Todo, Deadline and Event classes that inherits from Task

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,17 @@
+public class Deadline extends Task{
+
+    protected String by;
+    public Deadline(String description, String by){
+        super(description);
+        this.by = by;
+        this.symbol = "D";
+    }
+
+    public void setBy(String by) {
+        this.by = by;
+    }
+
+    public String getBy() {
+        return by;
+    }
+}
