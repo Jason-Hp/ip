9fc06c66236a1e7da871ec731176c5cdb42bb504
Jason-Hp <107877395+Jason-Hp@users.noreply.github.com>
2025-02-10 02:16:46 +0800

    Update Mike.java to hanlde exceptions
    Add try and catch blocks, and made existing methods throw exceptions if
    and when required like random commands or empty descriptions.

--- src/main/java/Mike.java
+++ src/main/java/Mike.java
@@ -19,7 +19,7 @@ public class Mike {
     public static final String LINE_SEPARATOR = "    ______________________________"+
             "______________________________";
 
-    public static String contains(String userInput){
+    public static String contains(String userInput) throws RandomException{
         if (userInput.contains("mark")){
             return "mark";
         } else if (userInput.contains("list")) {
@@ -31,23 +31,31 @@ public class Mike {
         } else if (userInput.contains("event")) {
             return "event";
         }
-        return "ERROR";
+        throw new RandomException();
     }
 
-    public static Todo todoParser(String userInput){
+    public static Todo todoParser(String userInput) throws EmptyException{
         String todo = userInput.substring(TODO_AND_SPACE);
+        if (todo.isEmpty()){
+            throw new EmptyException();
+        }
         return new Todo(todo);
     }
 
-    public static Deadline deadlineParser(String userInput){
+    public static Deadline deadlineParser(String userInput) throws EmptyException{
         String deadline = userInput.substring(DEADLINE_AND_SPACE);
+        if (deadline.isEmpty()){
+            throw new EmptyException();
+        }
         String[] deadlineSplit = deadline.split(" /by ");
         return new Deadline(deadlineSplit[0], deadlineSplit[1]);
     }
 
-    public static Event eventParser(String userInput){
+    public static Event eventParser(String userInput) throws EmptyException{
         String event = userInput.substring(EVENT_AND_SPACE);
-
+        if (event.isEmpty()){
+            throw new EmptyException();
+        }
         String[] eventSplit = event.split(" /from ");
         String[] fromAndToSplit = eventSplit[1].split(" /to ");
         return new Event(eventSplit[0], fromAndToSplit[0], fromAndToSplit[1]);
@@ -73,66 +81,95 @@ public class Mike {
                 LINE_SEPARATOR+"\n");
 
         String userInput = scanner.nextLine();
-        while (!userInput.equalsIgnoreCase("bye")) {
 
-            String command = contains(userInput);
+            while (!userInput.equalsIgnoreCase("bye")) {
 
-            switch (command) {
-            case "todo":
-                list[numberOfItems] = todoParser(userInput);
-                break;
-            case "deadline":
-                list[numberOfItems] = deadlineParser(userInput);
-                break;
-            case "event":
-                list[numberOfItems] = eventParser(userInput);
-                break;
-            case "list":
-                System.out.println(LINE_SEPARATOR+"\n" +
-                        SPACES+"Here are the tasks in your list:");
-                
-                for (int i = 0; i < numberOfItems; i++) {
-                    System.out.print(SPACES+(i + 1) + "." + itemToString(list[i]));
+                String command = "ERROR";
+                try {
+                    command = contains(userInput);
+                } catch (RandomException e) {
+                    System.out.println(LINE_SEPARATOR);
+                    System.out.println(SPACES+"Invalid command. Try again.");
+                    System.out.println(LINE_SEPARATOR+"\n");
                 }
-                
-                System.out.println(LINE_SEPARATOR+"\n");
 
+                switch (command) {
+                case "todo":
+                    try {
+                        list[numberOfItems] = todoParser(userInput);
+                        break;
+                    } catch (EmptyException e) {
+                        System.out.println(LINE_SEPARATOR);
+                        System.out.println(SPACES+"Description is empty. Try again.");
+                        System.out.println(LINE_SEPARATOR+"\n");
+                        userInput = scanner.nextLine();
+                        continue;
+                    }
+
+                case "deadline":
+                    try {
+                        list[numberOfItems] = deadlineParser(userInput);
+                        break;
+                    } catch (EmptyException e) {
+                        System.out.println(LINE_SEPARATOR);
+                        System.out.println(SPACES+"Description is empty. Try again.");
+                        System.out.println(LINE_SEPARATOR+"\n");
+                        userInput = scanner.nextLine();
+                        continue;
+                    }
+                case "event":
+                    try {
+                        list[numberOfItems] = eventParser(userInput);
+                        break;
+                    } catch (EmptyException e) {
+                        System.out.println(LINE_SEPARATOR);
+                        System.out.println(SPACES+"Description is empty. Try again.");
+                        System.out.println(LINE_SEPARATOR+"\n");
+                        userInput = scanner.nextLine();
+                        continue;
+                    }
+                case "list":
+                    System.out.println(LINE_SEPARATOR+"\n" +
+                            SPACES+"Here are the tasks in your list:");
+
+                    for (int i = 0; i < numberOfItems; i++) {
+                        System.out.print(SPACES+(i + 1) + "." + itemToString(list[i]));
+                    }
+
+                    System.out.println(LINE_SEPARATOR+"\n");
+
+                    userInput = scanner.nextLine();
+                    continue;
+                case "mark":
+                    int itemToMark = Integer.parseInt(userInput.split(" ")[1]) - 1;
+
+                    markOrUnmark(itemToMark, numberOfItems, list, Check.MARK);
+
+                    userInput = scanner.nextLine();
+                    continue;
+                case "unmark":
+                    int itemToUnmark = Integer.parseInt(userInput.split(" ")[1]) - 1;
+
+                    markOrUnmark(itemToUnmark, numberOfItems, list, Check.UNMARK);
+
+                    userInput = scanner.nextLine();
+                    continue;
+                case "ERROR":
+                    userInput = scanner.nextLine();
+                    continue;
+                }
+
+                System.out.println(LINE_SEPARATOR+"\n" +
+                        SPACES+"Got it. I've added this task:\n" +
+                        SPACES+
+                        itemToString(list[numberOfItems])+
+                        SPACES + "Now you have "+(numberOfItems+1)+" task in the list."+"\n"+
+                        LINE_SEPARATOR+"\n");
+
+                numberOfItems++;
                 userInput = scanner.nextLine();
-                continue;
-            case "mark":
-                int itemToMark = Integer.parseInt(userInput.split(" ")[1]) - 1;
-
-                markOrUnmark(itemToMark, numberOfItems, list, Check.MARK);
-
-                userInput = scanner.nextLine();
-                continue;
-            case "unmark":
-                int itemToUnmark = Integer.parseInt(userInput.split(" ")[1]) - 1;
-
-                markOrUnmark(itemToUnmark, numberOfItems, list, Check.UNMARK);
-
-                userInput = scanner.nextLine();
-                continue;
-            case "ERROR":
-
-                System.out.println(LINE_SEPARATOR);
-                System.out.println(SPACES+"Invalid command. Try again.");
-                System.out.println(LINE_SEPARATOR+"\n");
-
-                userInput = scanner.nextLine();
-                continue;
             }
 
-            System.out.println(LINE_SEPARATOR+"\n" +
-                    SPACES+"Got it. I've added this task:\n" +
-                    SPACES+
-                    itemToString(list[numberOfItems])+
-                    SPACES + "Now you have "+(numberOfItems+1)+" task in the list."+"\n"+
-                    LINE_SEPARATOR+"\n");
-
-            numberOfItems++;
-            userInput = scanner.nextLine();
-        }
 
         System.out.println(LINE_SEPARATOR+"\n" +
                 SPACES +"Bye. Hope to see you again soon!\n" +
